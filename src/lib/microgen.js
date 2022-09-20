90af4643903e86af053a64c473addca222d280fa
Muhammad Haris Abdurrahman <52352087+habrhm@users.noreply.github.com>
2022-09-21 02:53:30 +0700

    feat: update apiKey

--- src/lib/microgen.js
+++ src/lib/microgen.js
@@ -1,6 +1,6 @@
 import { MicrogenClient } from "microgen-v3-sdk";
 
 export const microgen = new MicrogenClient({
-  apiKey: '9c63ae8e-e4ef-409a-9514-9aae13730c2d',
+  apiKey: '5e6230e3-b486-4de9-9d14-2585a68edb92',
   url: "https://api.stagingv3.microgen.id",
 });
